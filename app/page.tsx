98781b64e269c8619e48c1b7fc92ddef48528731
Chris <victorchris73@gmail.com>
2025-07-12 05:11:40 +0100

    fix: problem with casing causing deployment error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import SmoothScrollWrapper from "./components/smooth-scroll-wrapper";
 import Navbar from "./components/navbar";
-import Hero from "./sections/hero";
-import Projects from "./sections/projects";
-import About from "./sections/about";
+import Hero from "./sections/Hero";
+import Projects from "./sections/Projects";
+import About from "./sections/About";
 import CTA from "./sections/cta";
 
 export default function Home() {
